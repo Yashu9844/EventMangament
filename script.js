2ecc0b80936af26b0f689b8096de52ccaced7cf4
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-04-11 22:41:35 +0530

    add gallery section

--- script.js
+++ script.js
@@ -80,3 +80,23 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+
+
+function galleryAnimation(){
+  const elements = document.querySelectorAll('.translate-y-10');
+    
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.remove('opacity-0', 'translate-y-10');
+            entry.target.classList.add('opacity-100', 'translate-y-0');
+          }
+        });
+      }, {
+        threshold: 0.1
+      });
+    
+      elements.forEach(el => observer.observe(el));
+}
+galleryAnimation();
